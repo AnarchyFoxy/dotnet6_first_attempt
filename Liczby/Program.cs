f7b1109983dc664e8a7d5c2f2afefa0a7f3246d9
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-09 22:58:29 +0100

    Added siezof operator for checking size of basic types

--- Liczby/Program.cs
+++ Liczby/Program.cs
@@ -16,3 +16,23 @@ Console.WriteLine("{0},\n{1},\n{2}", notacja_dziesietna, notacja_binarna, notacj
 
 Console.WriteLine($"{notacja_dziesietna == notacja_binarna}");
 Console.WriteLine($"{notacja_dziesietna == notacja_szesnastkowa}");
+
+Console.WriteLine($"Typ int zajmuje {sizeof(int)} bajtów i może przechowywać liczby z zakresu od {int.MinValue:N0} do {int.MaxValue:N0}.");
+Console.WriteLine($"Typ double zajmuje {sizeof(double)} bajtów i może przechowywać liczby z zakresu od {double.MinValue:N0} do {double.MaxValue:N0}.");
+Console.WriteLine($"Typ decimal zajmuje {sizeof(decimal)} bajtów i może przechowywać liczby z zakresu od {decimal.MinValue:N0} do {decimal.MaxValue:N0}.");
+
+// Output:
+/*
+ 23,
+-23,
+2.3,
+2.3
+2000000,
+2000000,
+2000000
+True
+True
+Typ int zajmuje 4 bajtó w i może przechowywać liczby z zakresu od -2,147,483,648 do 2,147,483,647.
+Typ double zajmuje 8 bajtó w i może przechowywać liczby z zakresu od -179,769,313,486,231,570,814,527,423,731,704,356,798,070,567,525,844,996,598,917,476,803,157,260,780,028,538,760,589,558,632,766,878,171,540,458,953,514,382,464,234,321,326,889,464,182,768,467,546,703,537,516,986,049,910,576,551,282,076,245,490,090,389,328,944,075,868,508,455,133,942,304,583,236,903,222,948,165,808,559,332,123,348,274,797,826,204,144,723,168,738,177,180,919,299,881,250,404,026,184,124,858,368 do 179,769,313,486,231,570,814,527,423,731,704,356,798,070,567,525,844,996,598,917,476,803,157,260,780,028,538,760,589,558,632,766,878,171,540,458,953,514,382,464,234,321,326,889,464,182,768,467,546,703,537,516,986,049,910,576,551,282,076,245,490,090,389,328,944,075,868,508,455,133,942,304,583,236,903,222,948,165,808,559,332,123,348,274,797,826,204,144,723,168,738,177,180,919,299,881,250,404,026,184,124,858,368.
+Typ decimal zajmuje 16 bajtó w i może przechowywać liczby z zakresu od -79,228,162,514,264,337,593,543,950,335 do 79,228,162,514,264,337,593,543,950,335.
+*/
