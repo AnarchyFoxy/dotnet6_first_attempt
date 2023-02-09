a1f8aec2afa3ec7b0c7568ff3394062ebc530c8f
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-09 21:59:03 +0100

    initial commit

---
+++ Liczby/Program.cs
@@ -0,0 +1,18 @@
+﻿uint liczba_calkowita = 23;
+
+int liczba_calkowita_ze_znakiem = -23;
+
+float liczba_rzeczywista = 2.3F;
+
+double inna_liczba_rzeczywista = 2.3;
+
+Console.WriteLine("{0},\n{1},\n{2},\n{3}", liczba_calkowita, liczba_calkowita_ze_znakiem, liczba_rzeczywista, inna_liczba_rzeczywista);
+
+int notacja_dziesietna = 2_000_000;
+int notacja_binarna = 0b_0001_1110_1000_0100_1000_0000;
+int notacja_szesnastkowa = 0x_001E_8480;
+
+Console.WriteLine("{0},\n{1},\n{2}", notacja_dziesietna, notacja_binarna, notacja_szesnastkowa);
+
+Console.WriteLine($"{notacja_dziesietna == notacja_binarna}");
+Console.WriteLine($"{notacja_dziesietna == notacja_szesnastkowa}");
---
+++ Slownictwo/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+//Console.WriteLine();
+
+//Console.WriteLine("Witaj, Astrydziu");
+
+//Console.WriteLine("Dnia {0:D} temperatura wynosi {1}oC.", DateTime.Today, -4);
+System.Data.DataSet ds;
+HttpClient client;
+
+Assembly? zestaw = Assembly.GetEntryAssembly();
+if (zestaw == null) return;
+
+foreach (AssemblyName nazwa in zestaw.GetReferencedAssemblies())
+{
+    var a = Assembly.Load(nazwa);
+    int liczba_metod = 0;
+
+    foreach (var t in a.DefinedTypes)
+    {
+        liczba_metod += t.GetMethods().Count();
+    }
+
+    Console.WriteLine(
+        "W zestawie {0} jest {1:N0} typów i {2:N0} metod.",
+        arg0: nazwa.Name,
+        arg1: a.DefinedTypes.Count(),
+        arg2: liczba_metod);
+}
+Console.ReadKey();
+
+//Output:
+/*
+W zestawie System.Runtime jest 0 typó w i 0 metod.
+W zestawie System.Data.Common jest 400 typó w i 7,038 metod.
+W zestawie System.Net.Http jest 429 typó w i 4,650 metod.
+W zestawie System.Linq jest 108 typó w i 1,141 metod.
+W zestawie System.Console jest 54 typó w i 676 metod.
+*/
+
+double wysokosc_w_metrach = 1.88;
+Console.WriteLine($"Zmienna {nameof(wysokosc_w_metrach)} ma wartość {wysokosc_w_metrach}");
