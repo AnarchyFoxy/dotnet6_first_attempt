9b08e4f37d42862bde4490be252a8c8e03079ccb
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-11 13:34:39 +0100

    Working with variables

--- Liczby/Program.cs
+++ Liczby/Program.cs
@@ -21,6 +21,40 @@ Console.WriteLine($"Typ int zajmuje {sizeof(int)} bajtów i może przechowywać 
 Console.WriteLine($"Typ double zajmuje {sizeof(double)} bajtów i może przechowywać liczby z zakresu od {double.MinValue:N0} do {double.MaxValue:N0}.");
 Console.WriteLine($"Typ decimal zajmuje {sizeof(decimal)} bajtów i może przechowywać liczby z zakresu od {decimal.MinValue:N0} do {decimal.MaxValue:N0}.");
 
+Console.WriteLine("Używanie liczb typu double:");
+
+float a = 0.1F;
+float b = 0.2F;
+
+if (a + b == 0.3)
+{
+    Console.WriteLine($"{a} + {b} jest równe 0.3");
+}
+else
+{
+    Console.WriteLine($"{a} + {b} NIE jest równe 0.3");
+}
+Console.WriteLine(a + b);
+
+Console.WriteLine("Korzystanie z liczb typu decimal:");
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} jest równe 0.3");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} NIE jest równe 0.3");
+}
+
+
+
+
+Console.ReadKey();
+
 // Output:
 /*
  23,
@@ -35,4 +69,7 @@ True
 Typ int zajmuje 4 bajtó w i może przechowywać liczby z zakresu od -2,147,483,648 do 2,147,483,647.
 Typ double zajmuje 8 bajtó w i może przechowywać liczby z zakresu od -179,769,313,486,231,570,814,527,423,731,704,356,798,070,567,525,844,996,598,917,476,803,157,260,780,028,538,760,589,558,632,766,878,171,540,458,953,514,382,464,234,321,326,889,464,182,768,467,546,703,537,516,986,049,910,576,551,282,076,245,490,090,389,328,944,075,868,508,455,133,942,304,583,236,903,222,948,165,808,559,332,123,348,274,797,826,204,144,723,168,738,177,180,919,299,881,250,404,026,184,124,858,368 do 179,769,313,486,231,570,814,527,423,731,704,356,798,070,567,525,844,996,598,917,476,803,157,260,780,028,538,760,589,558,632,766,878,171,540,458,953,514,382,464,234,321,326,889,464,182,768,467,546,703,537,516,986,049,910,576,551,282,076,245,490,090,389,328,944,075,868,508,455,133,942,304,583,236,903,222,948,165,808,559,332,123,348,274,797,826,204,144,723,168,738,177,180,919,299,881,250,404,026,184,124,858,368.
 Typ decimal zajmuje 16 bajtó w i może przechowywać liczby z zakresu od -79,228,162,514,264,337,593,543,950,335 do 79,228,162,514,264,337,593,543,950,335.
-*/
+
+Używanie liczb typu double:
+0.1 + 0.2 NIE jest ró wne 0.3
+ */
---
+++ Zmienne/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+object wysokosc = 1.84;
+object imie = "Astryda";
+Console.WriteLine($"{imie} ma {wysokosc} centymetrów wzrostu");
+
+//int dlugosc1 = imie.Length;
+int dlugosc2 = ((string)imie).Length;
+Console.WriteLine($"{imie} ma {dlugosc2} znaków");
+
+/*
+ Astryda ma 1.84 centymetrów wzrostu
+ Astryda ma 7 znaków
+*/
+
+dynamic cos = "Astryda";
+cos = 12;
+cos = new[] { 3, 5, 7 };
+
+Console.WriteLine($"Długość to {cos.Length}");
+/*
+ * Dł ugość to 3
+ */
+
+Console.ReadKey();
